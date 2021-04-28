81657b7ad4b869d3c93a8cd55ac2dcb59a43c0bb
lukasbutwithak <68748381+lukasbutwithak@users.noreply.github.com>
2021-04-27 23:50:22 -0700

    Incomplete terrain updater
    Pushing an incomplete terrain updater for Kevin to look at

---
+++ Elementary/Assets/Scripts/UpdateTerrain.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateTerrain : MonoBehaviour
+{
+    public Transform player;
+    public GameObject terrain;
+    bool connected;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == player)
+        {
+            connected = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.transform == player)
+        {
+            connected = false;
+        }
+    }
+
+    void Update()
+    {
+        if (connected)
+        {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            Ray ray = new Ray(transform.position, direction);
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.collider.transform == player)
+                {
+                    terrain.GetComponent<CreateTerrain>().Changetile("red");
+                }
+            }
+        }
+    }
+}
