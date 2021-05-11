f7278f8fe90f3a2b3e9dd88ba4a59a72edfc8369
alexmarozick <alexmarozick@gmail.com>
2021-05-10 21:05:37 -0700

    Added game over screen, buttons don't work yet though

---
+++ Elementary/Assets/Scripts/FallDetect.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// credit: Unity3D - Fall Detection and Moving the Player
+// https://www.youtube.com/watch?v=q-u5RHzFgv8&t=102s
+
+
+public class FallDetect : MonoBehaviour
+{
+    GameObject player;
+    public GameOverScreen GameOverScreen;
+    int pointTotal = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.transform.position.y <= .10) {
+            GameOverScreen.Setup(pointTotal);
+        }
+    }
+}
